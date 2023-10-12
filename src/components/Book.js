96ef9c87785747850d968645f9e34fa3d33ff47a
Riya Dutta <riya_dutta@carmax.com>
2023-10-12 18:02:24 -0400

    Added author name and  alternate image

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,7 +6,9 @@ const Book = ({ book, changeBookShelf }) => {
     return (
         <div className="book">
             <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                <div className="book-cover">
+                    <img className="book-cover" alt="Not Found" src={book.backgroundImage}/>
+                    </div> 
                 <div className="book-shelf-changer">
                     <select defaultValue={book.shelf ? book.shelf : "none"} onChange={(e) => changeBookShelf(book, e.target.value)}>
                         <option value="move" disabled>Move to...</option>
@@ -18,7 +20,7 @@ const Book = ({ book, changeBookShelf }) => {
                 </div>
             </div>
             <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.publisher}</div>
+            <div className="book-authors">{book.authors}</div>
         </div>
     )
 
